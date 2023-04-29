d4fdcd6d112fc6091e721c15f16164c9874dc178
nat2arsenal <nathanielarsenal@gmail.com>
2023-04-29 17:56:56 +0800

    Try to fix not working email send on github pages

--- app.js
+++ app.js
@@ -11,21 +11,3 @@
     document.body.classList.toggle('light-mode');
   });
 })();
-
-function SendMail() {
-  var params = {
-    from_name: document.getElementById('sender_name').value,
-    email: document.getElementById('sender_email').value,
-    subject: document.getElementById('email_subject').value,
-    message: document.getElementById('message').value,
-  };
-  emailjs
-    .send('service_iqd2onr', 'template_8s4wkpa', params)
-    .then(function (res) {
-      alert('Success with status code ' + res.status);
-      document.getElementById('sender_name').value = '';
-      document.getElementById('sender_email').value = '';
-      document.getElementById('email_subject').value = '';
-      document.getElementById('message').value = '';
-    });
-}
