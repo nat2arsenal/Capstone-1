1c99027166b1f99fbedf8b3fd3cc510a3e93b374
nat2arsenal <nathanielarsenal@gmail.com>
2023-04-29 20:03:01 +0800

    Contact input fields check

--- send_email.js
+++ send_email.js
@@ -5,13 +5,23 @@ function SendMail() {
     subject: document.getElementById('email_subject').value,
     message: document.getElementById('message').value,
   };
-  emailjs
-    .send('service_iqd2onr', 'template_8s4wkpa', params)
-    .then(function (res) {
-      alert('Success with status code ' + res.status);
-      document.getElementById('sender_name').value = '';
-      document.getElementById('sender_email').value = '';
-      document.getElementById('email_subject').value = '';
-      document.getElementById('message').value = '';
-    });
+
+  if (
+    params.from_name == '' ||
+    params.email == '' ||
+    params.subject == '' ||
+    params.message == ''
+  ) {
+    alert('Failed, please fill in all forms and resubmit again.');
+  } else {
+    emailjs
+      .send('service_iqd2onr', 'template_8s4wkpa', params)
+      .then(function (res) {
+        alert('Success with status code ' + res.status);
+        document.getElementById('sender_name').value = '';
+        document.getElementById('sender_email').value = '';
+        document.getElementById('email_subject').value = '';
+        document.getElementById('message').value = '';
+      });
+  }
 }
