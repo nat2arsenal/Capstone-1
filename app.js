0110d4bd3f0fd6b8e18b41bcc626d60cc01e91dd
nat2arsenal <nathanielarsenal@gmail.com>
2023-04-29 17:45:15 +0800

    Add emailing function on contact form

--- app.js
+++ app.js
@@ -11,3 +11,21 @@
     document.body.classList.toggle('light-mode');
   });
 })();
+
+function SendMail() {
+  var params = {
+    from_name: document.getElementById('sender_name').value,
+    email: document.getElementById('sender_email').value,
+    subject: document.getElementById('email_subject').value,
+    message: document.getElementById('message').value,
+  };
+  emailjs
+    .send('service_iqd2onr', 'template_8s4wkpa', params)
+    .then(function (res) {
+      alert('Success with status code ' + res.status);
+      document.getElementById('sender_name').value = '';
+      document.getElementById('sender_email').value = '';
+      document.getElementById('email_subject').value = '';
+      document.getElementById('message').value = '';
+    });
+}
