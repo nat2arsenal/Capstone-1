05b423979a4a3f4e415c524bf9cc9c99dbb3c451
nat2arsenal <nathanielarsenal@gmail.com>
2023-04-30 16:40:29 +0800

    Add animation to intro, put javascript code in one file

--- app.js
+++ app.js
@@ -10,4 +10,35 @@
   document.querySelector('.theme-btn').addEventListener('click', () => {
     document.body.classList.toggle('light-mode');
   });
+  document.querySelector('#button').addEventListener('click', () => {
+    var params = {
+      from_name: document.getElementById('sender_name').value,
+      email: document.getElementById('sender_email').value,
+      subject: document.getElementById('email_subject').value,
+      message: document.getElementById('message').value,
+    };
+
+    if (
+      params.from_name == '' ||
+      params.email == '' ||
+      params.subject == '' ||
+      params.message == ''
+    ) {
+      alert('Failed, please fill in all forms and click send again.');
+    } else {
+      emailjs
+        .send('service_iqd2onr', 'template_8s4wkpa', params)
+        .then(function (res) {
+          if (res.status === 200) {
+            alert('Email Sent Successfully!');
+            document.getElementById('sender_name').value = '';
+            document.getElementById('sender_email').value = '';
+            document.getElementById('email_subject').value = '';
+            document.getElementById('message').value = '';
+          } else {
+            alert('Sorry there was an error with status code' + res.status);
+          }
+        });
+    }
+  });
 })();
